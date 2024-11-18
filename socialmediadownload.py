5d5d7d91dde0322ef49e94da9df1d17c11ed17fa
bro2020 <65536615+bro2020@users.noreply.github.com>
2024-11-18 23:05:19 +0200

    Update socialmediadownload.py
    Use outtmpl '/tmp/%(title)s.%(ext)s',

--- socialmediadownload.py
+++ socialmediadownload.py
@@ -170,7 +170,7 @@ class SocialMediaDownloadPlugin(Plugin):
         data = json.loads(response_text.decode())
 
         ydl_opts = {
-            'outtmpl': '/tmp/%(id)s.%(ext)s',
+            'outtmpl': '/tmp/%(title)s.%(ext)s',
             'format': 'bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
             'geo-bypass': True,
             'nocheckcertificate': True,
