d75a8e9bf688e2eeb1332ccee70327298c3475c9
bro2020 <65536615+bro2020@users.noreply.github.com>
2025-04-01 01:22:21 +0300

    Update socialmediadownload.py, fix dict syntax

--- socialmediadownload.py
+++ socialmediadownload.py
@@ -172,10 +172,11 @@ class SocialMediaDownloadPlugin(Plugin):
 
             ydl_opts = {
                 'outtmpl': '%(title)s.%(ext)s',
-                'paths':
+                'paths': {
                     'default': '/tmp',
                     'home': '/tmp',
                     'temp': '/tmp'
+                }
                 'format': 'bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
                 'geo-bypass': True,
                 'nocheckcertificate': True,
