d8814d526fc3d194a9a41a858ed14ca64bcdcfb4
bro2020 <65536615+bro2020@users.noreply.github.com>
2025-04-01 01:24:33 +0300

    Update socialmediadownload.py, the same

--- socialmediadownload.py
+++ socialmediadownload.py
@@ -176,7 +176,7 @@ class SocialMediaDownloadPlugin(Plugin):
                     'default': '/tmp',
                     'home': '/tmp',
                     'temp': '/tmp'
-                }
+                },
                 'format': 'bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
                 'geo-bypass': True,
                 'nocheckcertificate': True,
